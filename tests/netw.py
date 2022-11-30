f21795925aab81b96ebfbe15456ca0a2656c3f54
Diego Lopez <ndzerglink@live.jp>
2022-11-30 16:56:04 +0900

    Added to netw.py loop to fetch network devs

--- tests/netw.py
+++ tests/netw.py
@@ -1,6 +1,17 @@
+"""
+Using Python and NetworkManager on GTK
+to control network
+"""
 import gi
 
 gi.require_version("NM","1.0")
 from gi.repository import GLib, NM
 client = NM.Client.new(None)
-print("version",client.get_version())
+# print("version",client.get_version())
+
+devices = client.get_devices()
+print("devices")
+for dev in devices:
+    print(" -name:",dev.get_iface())
+    print("  type:",dev.get_type_description())
+    print("  state:",dev.get_state().value_nick)
